b63b47b272d9f6cc7e6701399ab8c041d45992fa
Emmanuel Farrar <6923873+emmanuelfarrar@users.noreply.github.com>
2020-01-18 02:18:33 +0300

    Starting Event listeners
    Finish over food.

--- app/src/main/java/com/example/coolcalc/MainActivity.java
+++ app/src/main/java/com/example/coolcalc/MainActivity.java
@@ -2,7 +2,8 @@ package com.example.coolcalc;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+import android.widget.Button;
+import android.widget.ImageButton;
 
 
 public class MainActivity extends Activity {
@@ -11,5 +12,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Event listeners
+        Button oneBtn = findViewById(R.id.oneBtn);
+        Button twoBtn = findViewById(R.id.twoBtn);
+
+
+        ImageButton calcBtn = findViewById(R.id.calcBtn);
+        
+
     }
 }
